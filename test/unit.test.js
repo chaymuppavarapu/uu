0806e1f3b9b8e69562004066604c657c7ae597e9
chaymuppavarapu <158497709+chaymuppavarapu@users.noreply.github.com>
2025-03-25 15:37:25 +0530

    Create unit.test.js

---
+++ test/unit.test.js
@@ -0,0 +1,16 @@
+test/unit.test.js
+
+
+const assert = require('chai').assert;
+
+// Example JavaScript function in the static HTML file
+function greet() {
+  return 'Hello from Docker!';
+}
+
+describe('Unit Tests', () => {
+  it('should return the correct greeting message', () => {
+    const message = greet();
+    assert.equal(message, 'Hello from Docker!');
+  });
+});
